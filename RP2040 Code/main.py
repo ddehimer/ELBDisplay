d653d543a6af31664f7885cb3b9712435549da64
docile-10 <blakedoss@cedarville.edu>
2026-01-29 13:59:06 -0500

    Adding System Overview

--- RP2040 Code/main.py
+++ RP2040 Code/main.py
@@ -1,11 +1,22 @@
-from uart_link import send, receive
+from machine import UART, Pin
 import time
 
-print("RP2040 ready")
+uart = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
+
+def send(msg):
+    uart.write((msg + "\n").encode("utf-8"))
+
+def receive():
+    if uart.any():
+        data = uart.readline()
+        if isinstance(data, bytes):
+            return data.decode("utf-8").strip()
+    return None
+
+send("RP2040_READY")
 
 while True:
     cmd = receive()
     if cmd:
-        print("RX:", cmd)
         send("ECHO:" + cmd)
     time.sleep(0.1)
